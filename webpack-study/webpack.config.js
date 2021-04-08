51ff415ba7c5fff08f6921d3bda6cdd99fe0d906
yeoj1n <yeojinkim94@gmail.com>
2021-04-09 00:28:36 +0900

    feat: webpack loader 완료

--- webpack-study/webpack.config.js
+++ webpack-study/webpack.config.js
@@ -11,11 +11,31 @@ module.exports = {
     // 모든 js 파일에 대해서 my-webpack-loader가 실행됨.
     module: {
         rules: [
+            /*
             {
                 test: /\.js$/,
                 use: [
                     path.resolve('./my-webpack-loader.js')
                 ]
+            }
+            */
+           // loader 실행순서는 밑 -> 앞 (css-loader -> style-loader)
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader',
+                options: {
+                    publicPath: './dist/',
+                    name: '[name].[ext]?[hash]',
+                    // 2kb 이하는 문자열, 이상은 file로 변환
+                    limit: 20000 //20kb
+                }
             }
         ]
     }
